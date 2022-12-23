df456fdb0fab27d7764467746443dd45137e9b10
Gary <glswim@gmail.com>
2022-12-24 01:56:22 +0800

    added image files

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const btn = document.querySelector('.dropdown-btn');
+const menu = document.querySelector('.dropdown-menu');
 
 navToggle.addEventListener('click',() =>{
   primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) :
@@ -11,6 +13,16 @@ navToggle.addEventListener('click',() =>{
   primaryHeader.toggleAttribute("data-overlay");
 });
 
+btn.addEventListener('click', function(){
+  menu.classList.toggle('show');
+});
+
+document.addEventListener('click',function(event){
+  if (!event.target.matches('.dropdown-btn')){
+    menu.classList.remove('show');
+  }
+})
+
 // Translation
 
 var arrLang ={
@@ -18,14 +30,14 @@ var arrLang ={
         "TITLE": "Jim Royal Trading",
         "ABOUT": "About",
         "BRAND": "Brand",
-        "CONTACT": "Contact Us",
+        "CONTACT": "Contact",
         "NEWS": "News and Event",
         "HERO-INFO": "We are service-driven import and export pharmaceutical company"
     },
     "zh-tw":{
         "TITLE": "仁友興業有限公司",
         "ABOUT": "關於我們",
-        "PRODUCT": "產品",
+        "BRAND": "產品",
         "CONTACT": "聯絡我們",
         "NEWS": "新聞",
         "HERO-INFO": "我們是服務型進出口醫藥公司"
