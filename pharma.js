faa84c7ed908209406573db6183d18fe1140760b
xsymmetry <glswim@gmail.com>
2023-02-18 17:51:08 +0800

    Added a read function

--- pharma.js
+++ pharma.js
@@ -1,27 +1,29 @@
-const data = import('./productsdb.json', {
-    assert: {
-        type: 'json'
-    }
+$(document).ready(function(){
+    $.getJSON("productsdb.json", function(data){
+
+        data.forEach(item => {
+
+            const productContainer = document.querySelector('.product-cards');
+
+            const container = document.createElement('div');
+            container.classList.add('card');
+    
+            const productName = document.createElement("h3");
+            productName.textContent = item.product;
+            productName.classList.add('product-name');
+            container.appendChild(productName);
+        
+            const p = document.createElement("p");
+            p.classList.add('content');
+            p.textContent = item.description;
+            container.appendChild(p);
+    
+            productContainer.appendChild(container);
+        
+        });
+    }).fail(function(){
+        console.log("An error has occured.");
+    });
 });
 
-
-data.then(d =>
-    d.default.forEach(item => {
-        const productContainer = document.querySelector('.product-cards');
-
-        const container = document.createElement('div');
-        container.classList.add('card');
-
-        const productName = document.createElement("h3");
-        productName.textContent = item.product;
-        productName.classList.add('product-name');
-        container.appendChild(productName);
-    
-        const p = document.createElement("p");
-        p.classList.add('content');
-        p.textContent = item.description;
-        container.appendChild(p);
-
-        productContainer.appendChild(container);
-    }));
 
