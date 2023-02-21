a0bef3dd92968a66a47589fd8e5acc76a8915095
Gary <glswim@gmail.com>
2023-02-21 10:32:26 +0800

    updated code

--- pharma.js
+++ pharma.js
@@ -1,70 +1,151 @@
-// function appendProduct(name, desc){
-    
-//     const productContainer = document.querySelector('.product-cards');
-
-//     const container = document.createElement('div');
-//     container.classList.add('card');
-
-//     const productName = document.createElement("h3");
-//     productName.textContent = name;
-//     productName.classList.add('product-name');
-//     container.appendChild(productName);
-
-//     const p = document.createElement("p");
-//     p.classList.add('content');
-//     p.textContent = desc;
-//     container.appendChild(p);
-
-//     productContainer.appendChild(container);
-// }
-
-
-
 const nav = document.querySelectorAll(".alphabet");
-nav.forEach(item =>{
-    var getProduct = $.getJSON("productsdb.json", function(data){
-        var items = [];
-        data.forEach(item =>{
-            var name = item.product;
-            return name;
-        });
-        console.log("success")
-    }).done(function(){
-        console.log("complete");
-    }).fail(function(){
-        console.log("Something went wrong");
+var key = "all";
+function appendProduct(name, desc){
+    
+    const productContainer = document.querySelector('.product-cards');
+
+    const container = document.createElement('div');
+    container.classList.add('card');
+    // productName.classList.add('hidden');
+
+
+    const productName = document.createElement("h3");
+    productName.textContent = name;
+    productName.classList.add('product-name');
+    container.appendChild(productName);
+
+    const p = document.createElement("p");
+    p.classList.add('content');
+    p.textContent = desc;
+    container.appendChild(p);
+
+    productContainer.appendChild(container);
+}
+async function getData(){
+    const request = new Request('productsdb.json');
+
+    const response = await fetch(request);
+    const dataText = await response.text();
+
+    const data = JSON.parse(dataText);
+
+    data.forEach(element => {
+        appendProduct(element['product'], element['description']);
     });
-    
-    $(document).ready(function(){
-        $(".alphabet").click(function(event){
-            alert($(this).attr("key"));
-        });
-    })
-})
-// $(document).ready(function(){
-//     $(".alphabet").click(function(){
-//         var a = $(this).attr("key");
+}
+getData();
 
-//         $.getJSON("productsdb.json", function(data){
-
-//             data.forEach(item => {
-//                 if (item.product.slice(0,1).toLowerCase() == a)
-//                 {
-//                     // console.log(item.product);
-    
-//                     appendProduct(item.product, item.description);
-//                 }
-//                 else{
-//                     console.log("not match found");
-//                 }
-            
-//             });
-//         }).fail(function(){
-//             console.log("An error has occured.");
-//         });
-//     });
-
-  
+// const alphabetbtn = document.querySelectorAll('.alphabet');
+// alphabetbtn.forEach(button => {
+//     button.addEventListener("click", function(event)
+//     {
+//     if (element['product'].slice(0,1) == 'a'){
+//         appendProduct(element['product'], element['description']);            }
+//     else{
+//     console.log("false");
+//     }
 // });
 
+//-----------------------------JQuery-----------------------------------------------
+// $('.alphabet').click()
+// {
+//     console.log('clicked');
+// }
+// var array =[];
+// var obj = $.getJSON("productsdb.json", function() {
+//     console.log("success");
+// })
+//     .done(function(data)
+//     {
+//         $.each(data, function(key, value){
+//             appendProduct(value['product'], value['description'])
+//         })
+//     });
+    
+
+
+
+
+// function getKey(input){
+//     switch(input)
+//     {
+//         case "a":
+//             return "a";
+//             break;
+//         case "b":
+//             return "b";
+//             break;
+//         case "c":
+//             return "c";
+//             break;
+//         case "d":
+//             return "d";
+//             break;
+//         case "e":
+//             return "e";
+//             break;
+//         case "f":
+//             return "f";
+//             break;
+//         case "g":
+//             return "g";
+//             break;
+//         case "h":
+//             return "h";
+//             break;
+//         case "i":
+//             return"i";
+//             break;
+//         case "j":
+//             return"j";
+//             break;
+//         case "k":
+//             return"k";
+//             break;
+//         case "l":
+//             return"l";
+//             break;
+//         case "m":
+//             return"m";
+//             break;
+//         case "n":
+//             return"n";
+//             break;
+//         case "o":
+//             return "o";
+//             break;
+//         case "p":
+//             return "p";
+//             break;
+//         case "q":
+//             return "q";
+//             break;
+//         case "r":
+//             return "r";
+//             break;
+//         case "s":
+//             return "s";
+//             break;
+//         case "t":
+//             return "t";
+//             break;
+//         case "u":
+//             return "u";
+//             break;
+//         case "v":
+//             return "v";
+//             break;
+//         case "w":
+//             return "w";
+//             break;
+//         case "y":
+//             return "y";
+//             break;
+//         case "z":
+//             return "z";
+//             break;
+//         default:
+//             return "all";
+//     }
+// }
 
