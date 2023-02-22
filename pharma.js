7b4db50feadeb01e767959a7c36ed16acf6d2c1b
Gary <glswim@gmail.com>
2023-02-23 02:11:02 +0800

    updated code

--- pharma.js
+++ pharma.js
@@ -21,173 +21,94 @@ function appendProduct(name, desc){
 
     productContainer.appendChild(container);
 }
-function noResult(){
-    const container = document.createElement('div');
-    const text = document.createElement('h3');
-    text.textContent = "No result"
-    container.appendChild(text);
-}
-async function getData(){
-    const request = new Request('productsdb.json');
-
-    const response = await fetch(request);
-    const dataText = await response.text();
-
-    const data = JSON.parse(dataText);
-
-    data.forEach(element => {
-        appendProduct(element['product'], element['description']);
-    });
-}
-getData();
-function hideAll(){
-    const cards = document.querySelectorAll('.card');
-    cards.forEach(item => {
-        item.classList.add('hidden');
-    })
-}
-function getProduct(){
-    const arrayOfProduct= [];
-    const getProduct = document.querySelectorAll(".product-cards .card")
-    getProduct.forEach((item) =>{
-        let person = {product: item.children[0].innerHTML,
-                    description: item.children[1].innerHTML};
-        //Only save items that begins with 'a'
-        arrayOfProduct.push(person);
-    });
-    return(arrayOfProduct);
-   
-}
-const getAll = document.querySelector('.all');
-getAll.addEventListener('click', getData);
-
-const alphabetbtn = document.querySelectorAll('.alphabet');
-alphabetbtn.forEach(button => {
-    button.addEventListener("click", function(event)
-    {
-        let valid = 0;
-        let invalid = 0;
-        hideAll();
-        var key = (button.id);
-        getProduct().forEach((item, index) =>{
-            if(item.product.slice(0,1).toLowerCase() == key){
-                appendProduct(item.product, item.description);
-                valid++;
-            }
-            else{
-                invalid++;
-            }
-        });
-        if (valid == 0){
-            console.log("there are 0 valids")
-        }
-        else{
-            console.log(`There are ${invalid} hidden and ${valid} are valid`);
-        }
- 
-    });
-});
-
-//-----------------------------JQuery-----------------------------------------------
-// $('.alphabet').click()
-// {
-//     console.log('clicked');
+// function noResult(){
+//     const container = document.createElement('div');
+//     const text = document.createElement('h3');
+//     text.textContent = "No result"
+//     container.appendChild(text);
 // }
-// var array =[];
-// var obj = $.getJSON("productsdb.json", function() {
-//     console.log("success");
-// })
-//     .done(function(data)
-//     {
-//         $.each(data, function(key, value){
-//             appendProduct(value['product'], value['description'])
-//         })
+// async function getData(){
+//     const request = new Request('productsdb.json');
+
+//     const response = await fetch(request);
+//     const dataText = await response.text();
+
+//     const data = JSON.parse(dataText);
+
+//     data.forEach(element => {
+//         appendProduct(element['product'], element['description']);
 //     });
-    
-
-
-
-
-// function getKey(input){
-//     switch(input)
-//     {
-//         case "a":
-//             return "a";
-//             break;
-//         case "b":
-//             return "b";
-//             break;
-//         case "c":
-//             return "c";
-//             break;
-//         case "d":
-//             return "d";
-//             break;
-//         case "e":
-//             return "e";
-//             break;
-//         case "f":
-//             return "f";
-//             break;
-//         case "g":
-//             return "g";
-//             break;
-//         case "h":
-//             return "h";
-//             break;
-//         case "i":
-//             return"i";
-//             break;
-//         case "j":
-//             return"j";
-//             break;
-//         case "k":
-//             return"k";
-//             break;
-//         case "l":
-//             return"l";
-//             break;
-//         case "m":
-//             return"m";
-//             break;
-//         case "n":
-//             return"n";
-//             break;
-//         case "o":
-//             return "o";
-//             break;
-//         case "p":
-//             return "p";
-//             break;
-//         case "q":
-//             return "q";
-//             break;
-//         case "r":
-//             return "r";
-//             break;
-//         case "s":
-//             return "s";
-//             break;
-//         case "t":
-//             return "t";
-//             break;
-//         case "u":
-//             return "u";
-//             break;
-//         case "v":
-//             return "v";
-//             break;
-//         case "w":
-//             return "w";
-//             break;
-//         case "y":
-//             return "y";
-//             break;
-//         case "z":
-//             return "z";
-//             break;
-//         default:
-//             return "all";
-//     }
 // }
+// getData();
+const remove = document.getElementById('remove');
+function removeAll(){
+    const productCard = document.querySelector('.product-cards');
+    while(productCard.firstElementChild){
+        productCard.firstElementChild.remove();
+    }
+remove.addEventListener('click', removeAll);
 
+}
+
+// function getProduct(){
+//     const arrayOfProduct= [];
+//     const getProduct = document.querySelectorAll(".product-cards .card")
+//     getProduct.forEach((item) =>{
+//         let person = {product: item.children[0].innerHTML,
+//                     description: item.children[1].innerHTML};
+//         //Only save items that begins with 'a'
+//         arrayOfProduct.push(person);
+//     });
+//     return(arrayOfProduct);
+   
+// }
+// const getAll = document.querySelector('.all');
+// getAll.addEventListener('click', getData);
+
+// const alphabetbtn = document.querySelectorAll('.alphabet');
+// alphabetbtn.forEach(button => {
+//     button.addEventListener("click", function(event)
+//     {
+//         let valid = 0;
+//         let invalid = 0;
+//         hideAll();
+//         var key = (button.id);
+//         getProduct().forEach((item, index) =>{
+//             if(item.product.slice(0,1).toLowerCase() == key){
+//                 appendProduct(item.product, item.description);
+//                 valid++;
+//             }
+//             else{
+//                 invalid++;
+//             }
+//         });
+//         if (valid == 0){
+//             console.log("there are 0 valids")
+//         }
+//         else{
+//             console.log(`There are ${invalid} hidden and ${valid} are valid`);
+//         }
+ 
+//     });
+// });
+
+fetch('productsdb.json')
+    .then(response => response.json())
+    .then(data => {
+        let key;
+        const alphabetbtn = document.querySelectorAll('.alphabet');
+        removeAll();
+
+        alphabetbtn.forEach(button =>{
+            button.addEventListener('click', (e) =>{
+                key = button.id;
+                console.log(key);
+                const filtered = data.filter(item => item.product.toLowerCase().startsWith(key));
+
+                filtered.forEach(item => {
+                    appendProduct(item.product, item.description);
+                });
+            })
+        })
+    
+    })
